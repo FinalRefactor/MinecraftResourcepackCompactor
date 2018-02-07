aae7af2b81cee7bd692f09fff41670e960841098
Ranol <asdhugh1@gmail.com>
2018-02-07 22:20:36 +0900

    Improve JsonCompressor
    Integer optimizing (remove useless minus sign)
    Decimal optimizing (round up 3rd decimal place)
    Comment deleting (without copyright notice)

--- src/main/java/io/github/goldbigdragon/resourcepack/compactor/compressor/JsonCompressor.java
+++ src/main/java/io/github/goldbigdragon/resourcepack/compactor/compressor/JsonCompressor.java
@@ -17,13 +17,18 @@
 
 package io.github.goldbigdragon.resourcepack.compactor.compressor;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.common.io.MoreFiles;
+import com.google.gson.*;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 
 public class JsonCompressor implements FileCompressor {
     private final JsonParser jsonParser = new JsonParser();
@@ -35,9 +40,77 @@ public class JsonCompressor implements FileCompressor {
         try (BufferedReader reader = Files.newBufferedReader(path)) {
             parsed = jsonParser.parse(reader);
         }
+        if ("json".equals(MoreFiles.getFileExtension(path)) && parsed.isJsonObject()) {
+            // modeling file...
+            // todo: texture optimization
+            AtomicReference<JsonElement> result = new AtomicReference<>();
+            optimize(result::set, parsed, true);
+            parsed = result.get();
+        }
 
         try (BufferedWriter writer = Files.newBufferedWriter(path)) {
             gson.toJson(parsed, writer);
         }
+    }
+
+    private static boolean optimize(Consumer<JsonElement> set, JsonElement element, boolean root) {
+        if (element.isJsonPrimitive()) {
+            JsonPrimitive primitive = element.getAsJsonPrimitive();
+            if (!primitive.isNumber()) {
+                return false;
+            }
+            Number number = primitive.getAsNumber();
+            if (number.toString().indexOf('.') == -1) {
+                if (number.intValue() == -0) {
+                    set.accept(new JsonPrimitive(0));
+                    return true;
+                }
+                return false;
+            }
+            double doubleValue = number.doubleValue();
+            long longValue = number.longValue();
+            if (doubleValue == longValue) {
+                return false;
+            }
+            set.accept(new JsonPrimitive(
+                    new BigDecimal(String.valueOf(doubleValue)).setScale(2, RoundingMode.CEILING).floatValue()));
+            return true;
+        } else if (element.isJsonArray()) {
+            JsonArray array = new JsonArray();
+            for (JsonElement jsonElement : element.getAsJsonArray()) {
+                if (!optimize(array::add, jsonElement, false)) {
+                    array.add(jsonElement);
+                } else {
+                }
+            }
+            set.accept(array);
+            return true;
+        } else if (element.isJsonObject()) {
+            JsonObject original = element.getAsJsonObject();
+            JsonObject object = new JsonObject();
+            for (String key : original.keySet()) {
+                JsonElement jsonElement = original.get(key);
+                if ("__comment".equals(key)) {
+                    // keep author's copyright
+                    if (root) {
+                        object.add(key, jsonElement);
+                    }
+                    continue;
+                }
+                if (!optimize(it -> object.add(key, it), jsonElement, false)) {
+                    object.add(key, jsonElement);
+                }
+            }
+            set.accept(object);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String getConvertedTextureName(int order) {
+        String amount = Integer.toString(order / 26);
+        order %= 26;
+        return Character.toString((char) (order + 97)) + (amount.equals("0") ? "" : amount);
     }
 }
